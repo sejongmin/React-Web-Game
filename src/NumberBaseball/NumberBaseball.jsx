c27341cf18a5572327e9bd6f4104d0a9da3d297e
OhJongMin <jmoh7480@gmail.com>
2023-07-25 18:05:40 +0900

    [Edit] NumberBasevball

--- src/NumberBaseball/NumberBaseball.jsx
+++ src/NumberBaseball/NumberBaseball.jsx
@@ -2,11 +2,13 @@ import React, {useState, useRef} from "react";
 import './numberbaseball.css';
 
 const getNumber = () => {
-    let nums = []
-    for(let i = 0; i < 4; i++){
-        nums.push(Math.floor(Math.random() % 10))
+    const candidate = [0,1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for(let i = 0; i < 4; i += 1){
+        const chosen = candidate.splice(Math.floor(Math.random() * (10 - i)), 1)[0];
+        array.push(chosen);
     }
-    return nums;
+    return array;
 }
 
 const NumberBaseball = () => {
@@ -18,27 +20,78 @@ const NumberBaseball = () => {
         tries:[]
     });
 
-    const onSubmitForm = () => {
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        
+        if (state.value === state.answer.join('')){
+            setState({
+                ...state,
+                result:'홈런!',
+                tries:[...state.tries, {try : state.value, result: '홈런!'}],
+            })
+        } else{
+            const answerArray = state.value.split('').map((v) => {return parseInt(v) });
+            let strike = 0;
+            let ball = 0;
+            if (state.tries.length >= 9){
+                setState({
+                    ...state,
+                    result: `10번 넘게 틀려서 실패! 답은 ${state.answer} 입니다.`,
+                });
+            } else {
+                for (let i = 0; i < 4; i += 1){
+                    if (answerArray[i] === state.answer[i]){
+                        strike += 1;
+                    } else if (state.answer.includes(answerArray[i])){
+                        ball += 1;
+                    }
+                }
+                setState({
+                    ...state,
+                    tries : [...state.tries, {try : state.value, result : `${strike} 스트라이크, ${ball} 볼입니다. `}],
+                    value : '',
+                    result : `${strike} 스트라이크, ${ball} 볼입니다. `
+                })
+            }
+        }
         inputRef.current.focus();
     }
 
     const onChangeInput = (e) => {
         setState({
             ...state,
-            value : e.currentTarget.value
+            value : e.target.value
         });
     }
+
+    const tryTag = state.tries.map((v, i) => {
+        return(
+            <li key={i}>
+                <strong>{v.try}</strong><br />
+                <span>{v.result}</span>
+            </li>
+        )
+    })
+
+    const onClick = (e) => {
+        setState({
+            result:'',
+            value:'',
+            answer:getNumber(),
+            tries:[],
+        })
+    }
+
 
     return(
         <div className="NumberBaseball">
             <h1>{state.result}</h1>
             <form onSubmit={onSubmitForm}>
-                <input maxLength={4} value={state.value} onChange={onChangeInput}/>
+                <input ref={inputRef} minLength={4} maxLength={4} value={state.value} onChange={onChangeInput}/>
+                <button type="submit">입력</button>
+                <button type="button" onClick={onClick}>다시시작</button>
             </form>
             <div>시도:{state.tries.length}</div>
-            <ul>{}</ul>
+            <ul>{tryTag}</ul>
         </div>
     );
 }
